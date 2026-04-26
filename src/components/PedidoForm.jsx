5be7d2dec72927fbef3f7c0a12cadf12a1a8ed07
nrojascaycho-ops <nrojascaycho@gmail.com>
2026-04-26 06:42:19 -0500

    mejorando formulario

--- src/components/PedidoForm.jsx
+++ src/components/PedidoForm.jsx
@@ -3,40 +3,65 @@ import { useState } from "react";
 export default function PedidoForm({ onAdd }) {
   const [nombre, setNombre] = useState("");
   const [producto, setProducto] = useState("");
-  const [estado, setEstado] = useState("pendiente");
+  const [estado, setEstado] = useState(""); // 🔥 vacío por defecto
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!nombre || !producto) return;
+
+    // 🔥 VALIDAR TODO
+    if (!nombre || !producto || !estado) {
+      alert("Completa todos los campos");
+      return;
+    }
 
     onAdd({ nombre, producto, estado });
 
     setNombre("");
     setProducto("");
-    setEstado("pendiente");
+    setEstado("");
   };
 
   return (
-    <form className="form-card" onSubmit={handleSubmit}>
-      <input
-        placeholder="Nombre"
-        value={nombre}
-        onChange={e => setNombre(e.target.value)}
-      />
+    <div className="form-card">
+      <form onSubmit={handleSubmit} style={{ width: "100%" }}>
 
-      <input
-        placeholder="Producto"
-        value={producto}
-        onChange={e => setProducto(e.target.value)}
-      />
+        <h3>Rellene el formulario</h3>
 
-      <select value={estado} onChange={e => setEstado(e.target.value)}>
-        <option value="pendiente">Pendiente</option>
-        <option value="entregado">Entregado</option>
-        <option value="cancelado">Cancelado</option>
-      </select>
+        {/* NOMBRE */}
+        <label>Nombre</label>
+        <input
+          placeholder="Ingrese nombre"
+          value={nombre}
+          onChange={(e) => setNombre(e.target.value)}
+        />
 
-      <button className="btn-save">Guardar</button>
-    </form>
+        {/* PRODUCTO */}
+        <label>Producto</label>
+        <input
+          placeholder="Ingrese producto"
+          value={producto}
+          onChange={(e) => setProducto(e.target.value)}
+        />
+
+        {/* ESTADO */}
+        <label>Estado</label>
+        <select
+          value={estado}
+          onChange={(e) => setEstado(e.target.value)}
+        >
+          <option value="">Seleccione estado</option>
+          <option value="pendiente">Pendiente</option>
+          <option value="entregado">Entregado</option>
+          <option value="cancelado">Cancelado</option>
+        </select>
+
+        <br />
+
+        <button className="btn-save">
+          Guardar
+        </button>
+
+      </form>
+    </div>
   );
 }
