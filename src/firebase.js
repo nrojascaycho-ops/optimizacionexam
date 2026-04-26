33067d673a0a17726e31448cddc95865fb6dd119
nrojascaycho-ops <nrojascaycho@gmail.com>
2026-04-26 11:20:55 -0500

    ya porfin

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,18 @@
+// firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAvLLtArb1CylnDaCABB21vTBx8wja2syA",
-  authDomain: "gestion-pedidos-fa99a.firebaseapp.com",
-  projectId: "gestion-pedidos-fa99a",
-  storageBucket: "gestion-pedidos-fa99a.firebasestorage.app",
-  messagingSenderId: "44189358527",
-  appId: "1:44189358527:web:62654119b8425f06a1a90a"
+  apiKey: "AIzaSyCbgB1n8XXRND-E2oJTE5jyT6tgE_DYUz8",
+  authDomain: "pedidos-app-3e8f0.firebaseapp.com",
+  projectId: "pedidos-app-3e8f0",
+  storageBucket: "pedidos-app-3e8f0.firebasestorage.app",
+  messagingSenderId: "16995408994",
+  appId: "1:16995408994:web:68e84a3acde7ac8b44053b"
 };
 
+// 🔥 INICIALIZAR
 const app = initializeApp(firebaseConfig);
 
+// 🔥 CONECTAR FIRESTORE (LO QUE TE FALTABA)
 export const db = getFirestore(app);
